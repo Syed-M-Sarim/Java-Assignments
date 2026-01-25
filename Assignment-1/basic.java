37044055268bd7d4c376a5a46aec55385f1dc26e
Syed-M-Sarim <iamsarim1000@gmail.com>
2026-01-25 20:16:41 +0500

    Basic questions

---
+++ Assignment-1/basic.java
@@ -0,0 +1,9 @@
+public class basic{
+    public static void main(String []args){
+        int a = 15;
+        int b = 10;
+        int c = 20;
+        int avg = (a+b+c)/3;
+        System.out.println("Average: " + avg);
+    }
+}
